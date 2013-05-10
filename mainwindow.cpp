1fbdb50a6a973000f3faaa8e058b131da5e5b20c
Cyril Leroux <cyril0leroux@gmail.com>
2013-05-10 17:47:13 +0200

    Added close buttons on each tab

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,7 @@
 
 #include <QFileDialog>
 #include <QPushButton>
+#include <memory>
 
 MainWindow::MainWindow(QWidget *parent) :
     QWidget(parent)
@@ -10,13 +11,23 @@ MainWindow::MainWindow(QWidget *parent) :
     initLayout();
 }
 
+MainWindow::~MainWindow()
+{
+    delete(m_tabs);
+    m_tabs = nullptr;
+    delete(m_menuBar);
+    m_menuBar = nullptr;
+    delete(m_vLayout);
+    m_vLayout = nullptr;
+}
+
 void MainWindow::initMenu()
 {
     m_menuBar = new MenuBar();
 
     // Bind actions
     connect(m_menuBar, SIGNAL(openTriggered()),     this, SLOT(openFilesDialog()));
-    connect(m_menuBar, SIGNAL(closeTabTriggered()), this, SLOT(closeTab()));
+    connect(m_menuBar, SIGNAL(closeTabTriggered()), this, SLOT(closeCurrentTab()));
     connect(m_menuBar, SIGNAL(quitTriggered()),     this, SLOT(close()));
 
     connect(m_menuBar, SIGNAL(zoomInTriggered()),   this, SLOT(zoomIn()));
@@ -25,10 +36,14 @@ void MainWindow::initMenu()
 
 void MainWindow::initLayout()
 {
+    resize(600, 400);
+
     m_vLayout = new QVBoxLayout();
     m_vLayout->setContentsMargins(0, 0, 0, 0);
 
     m_tabs = new QTabWidget();
+    m_tabs->setTabsClosable(true);
+    connect(m_tabs, SIGNAL(tabCloseRequested(int)), this, SLOT(closeTab(int)));
 
     m_vLayout->addWidget(m_tabs);
     m_vLayout->setMenuBar(m_menuBar);
@@ -43,7 +58,8 @@ TabItem* MainWindow::currentTab() const
 
 void MainWindow::openFilesDialog()
 {
-    auto fileDialog = new QFileDialog(this);
+    auto fileDialog = std::unique_ptr<QFileDialog>(new QFileDialog(this));
+
     fileDialog->setFileMode(QFileDialog::ExistingFiles);
     fileDialog->setViewMode(QFileDialog::Detail);
     fileDialog->setFilter(QDir::Files);
@@ -62,19 +78,25 @@ void MainWindow::openFilesDialog()
 void MainWindow::newTab(const QString& filename)
 {
     auto newTab = new TabItem(filename);
-    const auto index = m_tabs->addTab(newTab, newTab->label());
-
-    auto closeBtn = new QPushButton(this);
-    m_tabs->tabBar()->setTabButton(index, QTabBar::RightSide, closeBtn);
+    m_tabs->addTab(newTab, newTab->label());
 }
 
-void MainWindow::closeTab()
+void MainWindow::closeTab(const int& index)
 {
-    const auto currentIndex = m_tabs->currentIndex();
-    if (currentIndex != -1) {
-        m_tabs->removeTab(currentIndex);
+    if (index == -1) {
+        return;
     }
 
+    auto tabItem = m_tabs->widget(index);
+    m_tabs->removeTab(index);
+
+    delete(tabItem);
+    tabItem = nullptr;
+}
+
+void MainWindow::closeCurrentTab()
+{
+    closeTab(m_tabs->currentIndex());
 }
 
 void MainWindow::zoomIn()
--- mainwindow.h
+++ mainwindow.h
@@ -14,6 +14,7 @@ class MainWindow : public QWidget
     Q_OBJECT
 public:
     explicit MainWindow(QWidget *parent = 0);
+    virtual ~MainWindow();
 
 private:
     void initMenu();
@@ -31,7 +32,8 @@ signals:
     
 public slots:
     void openFilesDialog();
-    void closeTab();
+    void closeTab(const int& index);
+    void closeCurrentTab();
     void zoomIn();
     void zoomOut();
     
